642caea3b95d662597e3bf08b0ae5e1a5bbdd8ba
Karl Jugapuu <98745340+KarlJug@users.noreply.github.com>
2023-10-25 22:01:21 +0300

    Add files via upload

--- Oskuste ÖÖ/player/Character.gd
+++ Oskuste ÖÖ/player/Character.gd
@@ -1,81 +1,119 @@
 extends CharacterBody2D
 
+@onready var dash_raycast = $RayCast2D
 
 var area1 : Area2D
 var original_pos : Vector2
+var dash_raycast_pos : Vector2
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -1300.0
 var is_dashing = false
 
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
-func player_dash(dash_direction):
+func player_dash():
 	if is_dashing:
 		return
 	
-	const x_speed = 3600
-	const y_speed = 1500  # 1200
-	
-	velocity.x = dash_direction.x * x_speed
-	velocity.y = dash_direction.y * y_speed
 	is_dashing = true
-	await get_tree().create_timer(0.13).timeout
+	
+	# Calculate the dash direction based on the mouse cursor position
+	var dash_vector = get_global_mouse_position() - global_position
+	dash_vector  = dash_vector.normalized()
+	
+	var vec_x = 1
+	if dash_vector.x < 0:
+		vec_x = -1
+		
+	else:
+		vec_x = 1
+	
+	# ray cast flip
+	dash_raycast.position.x = vec_x * dash_raycast_pos.x
+
+	if dash_raycast.rotation == 0 and vec_x == -1:
+		dash_raycast.rotation = 3
+	elif dash_raycast.rotation == 3 and vec_x == 1:
+		dash_raycast.rotation = 0
+	
+	var step = 0
+	var distance = 10
+	while 20 > step:
+		if is_on_wall() or dash_raycast.is_colliding():
+			break
+		
+		self.position.x += vec_x * distance
+
+		
+		await get_tree().create_timer(0.00000000000000000000000001).timeout  # smoothness
+		step += 1
+	
+	is_dashing = false
+
+
+func _on_dash_completed():
 	is_dashing = false
 
 
 func _ready():
+	dash_raycast_pos = dash_raycast.position
 	area1 = $area1
 	if area1:
 		original_pos = area1.position
 		
 	else:
 		print("area not found")
+		return 1
 
 
 func _physics_process(delta):
-	
-	#print(is_dashing)
-	# mouse pos for dash
-	var mouse_pos = get_global_mouse_position()
-	var dash_direction = (mouse_pos - global_position).normalized()
-	
-	#print(dash_direction)
-	
-	if is_on_floor():
-		is_dashing = false
-	
 	if Input.is_action_just_pressed("ui_mouse_dash"):
-		player_dash(dash_direction)
+		player_dash()
 		
 	# Handle Jump.
-	elif Input.is_action_just_pressed("ui_up") and is_on_floor():
+	elif Input.is_action_just_pressed("ui_up") and is_on_floor() and not $area1.attacking:
 		velocity.y = JUMP_VELOCITY
 
 	
 	# Add the gravity.
-	if not is_on_floor() and not is_dashing:
+	if not is_on_floor():
 		velocity.y += (gravity + 4000) * delta
+		if velocity.y < 0:
+			$AnimatedSprite2D.play("jump")
+		if velocity.y > 0:
+			$AnimatedSprite2D.play("falling")
 
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction and not is_dashing or is_on_floor():
-		if Input.is_action_just_pressed("ui_mouse_dash"):
-			player_dash(dash_direction)
+	if direction and not $area1.attacking:
+		if Input.is_action_just_pressed("ui_mouse_dash") and not is_dashing:
+			player_dash()
 			
 		elif not is_dashing:
 			velocity.x = direction * SPEED
-		
+			
+			# running animation flip logic and play
+			if direction < 0:
+				$AnimatedSprite2D.flip_h = true
+				if is_on_floor():
+					$AnimatedSprite2D.play("running")
+			elif direction > 0:
+				$AnimatedSprite2D.flip_h = false
+				if is_on_floor():
+					$AnimatedSprite2D.play("running")
+				
 			# attack hitbox flip
-			area1.position.x = direction * (original_pos.x * 5)
-			#print(original_pos)
-		
-		
+			area1.position.x = direction * original_pos.x
 	
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+		# idle animation
+		if not $area1.attacking and is_on_floor():
+			$AnimatedSprite2D.play("idle")
 
 	move_and_slide()
--- Oskuste ÖÖ/player/playerAttack.gd
+++ Oskuste ÖÖ/player/playerAttack.gd
@@ -3,14 +3,26 @@ extends Area2D
 @onready var area1 = self
 var enemy_scene = preload("res://enemy/enemy.tscn")
 @onready var enemy = enemy_scene.instantiate()
+@onready var animated_sprite_2d = $"../AnimatedSprite2D"
 var damage = 50
+var attacking = false
 
 func check_for_hits():
 	for area in area1.get_overlapping_areas():
 		if area.is_in_group("enemies"):
-			print("yes sir")
-			enemy.get_hit(damage)
-	
+			area.get_parent().get_hit(damage)
+
+
+func attack():
+	# animation
+		attacking = true
+		animated_sprite_2d.play("attack")
+		
+		# check if attack connects
+		check_for_hits()
+		await get_tree().create_timer(0.5).timeout
+		attacking = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -20,8 +32,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	# attack
-	if Input.is_action_just_pressed("ui_mouse_attack"):
-		# aniumation
-		
-		# check if attack connects
-		check_for_hits()
+	if Input.is_action_just_pressed("ui_mouse_attack") and not attacking:
+		attack()
+
+
+func _on_animated_sprite_2d_animation_finished():
+	animated_sprite_2d.play("idle")
