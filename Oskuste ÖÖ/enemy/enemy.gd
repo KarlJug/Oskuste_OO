2bd8805497647c044421013cf4359d4dea8861d7
Karl Jugapuu <98745340+KarlJug@users.noreply.github.com>
2023-10-26 11:46:09 +0300

    Add files via upload

--- Oskuste ÖÖ/enemy/enemy.gd
+++ Oskuste ÖÖ/enemy/enemy.gd
@@ -1,10 +1,13 @@
 extends CharacterBody2D
-
+@onready var player_scene = $"../player"
 @onready var ray_cast_2d = $RayCast2D
 @onready var ray_cast_attack = $RayCastAttack
+@onready var area_2 = $area2
+@onready var damage_area = $damageArea
 
 var ray_cast_pos : Vector2
 var ray_cast_attack_pos : Vector2
+
 
 var elud = 100
 var facing = true
@@ -12,12 +15,42 @@ var turn = false
 var moving_right = true
 var direction_x = 1
 var SPEED = 70
-
-
+var is_dead = false
+var is_attacking = false
+var is_dashing = false
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+
+func dash_attack():
+	if is_dashing:
+		return
+	
+	is_dashing = true
+	
+	var step = 0
+	var distance = 10
+	while 20 > step:
+		if not ray_cast_2d.is_colliding() or is_on_wall() or is_dead or not is_attacking:
+			break
+		
+		self.position.x += direction_x * distance
+		
+		await get_tree().create_timer(0.04).timeout  # smoothness
+		step += 1
+	
+	is_dashing = false
+
+
 func die():
+	# layers
+	# Remove the specific mask you want to stop checking (e.g., mask 4)
+	var new_collision_mask = 1 << 2  # For mask 2
+	
+	# Assign the updated collision mask back to the node
+	self.collision_mask &= ~new_collision_mask
+	area_2.collision_mask &= ~new_collision_mask
+	damage_area.collision_mask &= ~new_collision_mask
 	# animation
 	$AnimatedSprite2D.play("die")
 	await get_tree().create_timer(2).timeout
@@ -27,6 +60,9 @@ func die():
 func get_hit(damage):
 	elud -= damage
 	if elud <= 0:
+		
+		is_dead = true
+		$AnimatedSprite2D.stop()
 		die()
 
 
@@ -34,7 +70,7 @@ func idle():
 	turn = true
 	var turns = 0
 	
-	while 2 >= turns:
+	while 2 >= turns and not is_dead and not is_attacking:
 		$AnimatedSprite2D.play("idle")
 		match facing:
 			true:
@@ -56,16 +92,27 @@ func _ready():
 	ray_cast_pos = ray_cast_2d.position
 	ray_cast_attack_pos = ray_cast_attack.position
 	
-	$AnimatedSprite2D.play("Idle")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if is_on_wall():
+		pass
+	
+	if ray_cast_attack.is_colliding():
+		if ray_cast_attack.get_collider().get_name() == "player" and not is_dead:
+			is_attacking = true
+			await get_tree().create_timer(1.7).timeout
+			dash_attack()
+		
+	else:
+		is_attacking = false
+	
 	if not is_on_floor():
 		velocity.y += (gravity + 4000) * delta
 	
 	# movment and turn logic
-	if not ray_cast_2d.is_colliding() or is_on_wall():
+	if not is_dead and not is_attacking and not ray_cast_2d.is_colliding() or is_on_wall():
 		if direction_x < 0:
 			direction_x = 1
 		else:
@@ -75,11 +122,34 @@ func _process(delta):
 		ray_cast_attack.position.x *= -1  # direction_x * ray_cast_attack_pos.x
 		ray_cast_attack.scale.x *= -1
 		idle()
+		await get_tree().create_timer(1.7).timeout
 	
-	if not turn:
+	if not turn and not is_attacking and not is_dead:
 		velocity.x = direction_x * SPEED
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
 	move_and_slide()
+
+
+func _on_damage_area_body_entered(body):
+	print(global_position)
+	if body.get_name() == "player" and not is_dead:
+			print("hit")
+			var direction = (global_position - body.global_position).normalized()
+			var explosionForce = direction * body.explosionForce
+			body.elud -= 10
+			print(body.elud)
+			
+			var step = 0  # dont change
+			var max_steps = 4
+			explosionForce /= max_steps
+			while max_steps > step:
+				if body.is_on_wall() or body.is_on_ceiling():
+					break
+				
+				body.position -= explosionForce
+				
+				await get_tree().create_timer(0.007).timeout  # smoothness
+				step += 1
